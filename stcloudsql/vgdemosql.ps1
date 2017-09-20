f3498e5654e91f994576573f390bd413805559c7
tuggernuts77 <bryan.long@vespa-group.com>
2017-09-20 14:57:08 -0400

    Updated vgdemosql.ps1 in Azure Automation.

---
+++ stcloudsql/vgdemosql.ps1
@@ -0,0 +1,44 @@
+workflow bryanlongsqldemo {
+	inlineScript {
+        $connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+		$connectionstring="Server=tcp:bryanlongsql1.database.windows.net,1433;Initial Catalog=bryanlongvgdemo;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+		$myCredential=Get-AutomationPSCredential -Name 'vgdemosql'
+		$SQLUser= $myCredential.UserName
+		($SQLUserPassword= $myCredential.Password).MakeReadOnly()
+		$query="Select * from test"
+        $query2="ALTER TABLE [bryanlongvgdemo].[dbo].[test] ALTER COLUMN [DueDate] varchar(200) NOT NULL"
+		$Connection=New-Object System.Data.SQLClient.SQLConnection
+		$connection.ConnectionString = $connectionstring
+		$sqlcred=New-Object -TypeName System.Data.SqlClient.SqlCredential($SQLUser, $SQLUserPassword)
+		$connection.Credential = $sqlCred
+		$connection.Open()
+		$command=$connection.CreateCommand()
+		$command.CommandText=$query
+        $command.CommandText=$query2
+		$command.ExecuteReader()
+		$connection.Close()
+	}
+}
